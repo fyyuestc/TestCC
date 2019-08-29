dee7330b46874f32031739cbc48dd18bd08f66e1
fyy <564987299@qq.com>
2019-08-29 14:53:09 +0800

    Initial C work

---
+++ cmake-build-debug/CMakeFiles/TestCC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCC.dir/main.c.obj"
+  "TestCC.pdb"
+  "TestCC.exe"
+  "TestCC.exe.manifest"
+  "libTestCC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

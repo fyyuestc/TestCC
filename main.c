0ae5d67d0da6e50bfcc437b84a0e8d2ed85d427b
fyy <564987299@qq.com>
2019-08-29 18:59:48 +0800

    Initial C work

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+char *myFunc(){
+    char testChar = 'b';
+    char *myPo = &testChar;
+    return myPo;
+}
 
 int main() {
-    printf("Hello, World!\n");
+//    printf("%p",myFunc());
+    int test1 = 10;
+    int* const p = &test1;
+    *p = 20;
+    printf("%d", *p);
     return 0;
 }
